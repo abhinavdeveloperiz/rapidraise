dc50f398e721b379b557495e66ed91d617c1b336
abhinavmabhi <dexmarketing192018@gmail.com>
2026-02-13 16:24:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def blog(request):
+    return render(request,'blog.html')
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def contact(request):
+    return render(request, "contact.html")
